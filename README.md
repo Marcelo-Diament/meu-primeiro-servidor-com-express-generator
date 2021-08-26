9cd0a1ffe6e389ef9bb6f25484cb4f95b3d9611c
Marcelo-Diament <marcelodiament@gmail.com>
2021-08-25 21:34:24 -0300

    feature: brincando um pouco com o projeto [0]

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# meu-primeiro-servidor-com-express-generator
+# Express com EJS
+
 Projeto para aprendermos a trabalhar com o Express Generator e com EJS.
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    titulo: 'do Jefferson',
+    subtitulo: 'Site maneiro'
+  });
 });
 
 module.exports = router;
