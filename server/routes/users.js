ef0dd16c40bd58c0d6d3e28c30d1169d122d57d7
Marcelo-Diament <marcelodiament@gmail.com>
2021-08-25 21:53:52 -0300

    feature: users route, controller and view

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var userController = require('../controllers/users')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', userController.index);
 
 module.exports = router;
