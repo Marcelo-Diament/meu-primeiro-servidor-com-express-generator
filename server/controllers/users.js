1c5bb01a0b148306c3bf8debcc093715f049d15b
Marcelo-Diament <marcelodiament@gmail.com>
2021-08-30 21:47:57 -0300

    feature: adicionando view que percorre array de users

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,21 +1,44 @@
+const usersPlaceholder = [
+  {
+    id: 0,
+    nome: 'Fulano',
+    sobrenome: 'de Tal',
+    avatar: '/images/fulano.png'
+  },
+  {
+    id: 1,
+    nome: 'Beltrano',
+    sobrenome: 'de Tal',
+    avatar: '/images/beltrano.png'
+  },
+  {
+    id: 2,
+    nome: 'Ciclana',
+    sobrenome: 'de Tal',
+    avatar: '/images/ciclana.png'
+  },
+  {
+    id: 3,
+    nome: 'Abirosvaldo',
+    sobrenome: 'de Tal',
+    avatar: '/images/abirosvaldo.png'
+  }
+]
+
 const controller = {
   index: (req, res, next) => {
     res.render('users', {
       titulo: 'Usuários',
-      subtitulo: 'do site do Jefferson'
+      subtitulo: 'do site do Jefferson',
+      usuarios: usersPlaceholder
     });
   },
   show: (req, res, next) => {
-    const { apelido } = req.params
+    const { id } = req.params
     res.render('users', {
-      titulo: `Usuário ${apelido}`,
-      subtitulo: 'no site do Jefferson'
-    });
-  },
-  chuparCanaEAssobiar: (req, res, next) => {
-    res.render('users', {
-      titulo: 'Chupando cana',
-      subtitulo: '...e assobiando!'
+      titulo: 'Usuários',
+      subtitulo: 'do site do Jefferson',
+      usuarios: [usersPlaceholder[id]]
     });
   }
 }
--- server/routes/users.js
+++ server/routes/users.js
@@ -2,8 +2,7 @@ const express = require('express'),
   router = express.Router(),
   userController = require('../controllers/users')
 
-  router.get('/doideira', userController.chuparCanaEAssobiar)
-  router.get('/:apelido', userController.show)
   router.get('/', userController.index)
+  router.get('/:id', userController.show)
 
 module.exports = router
